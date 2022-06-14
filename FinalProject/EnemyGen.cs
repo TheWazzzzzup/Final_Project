4d3a9fe027d3194b529a434e1a008460035919e0
TheWazzzzzup <amitmoryosef@hotmail.com>
2022-06-14 17:06:04 +0300

    Update EnemyGen.cs
    Done With

--- FinalProject/EnemyGen.cs
+++ FinalProject/EnemyGen.cs
@@ -6,11 +6,41 @@ using System.Threading.Tasks;
 
 namespace FinalProject
 {
-    static class EnemyGen
+    class EnemyGen
     {
-        static string[] EnemyName = new string[8] {"Crow","Cat","Bat","","","","",""};
+        private int _damage;
+        private string _name;
+
+        private string[] EnemyName = {"Bat","Crow","Leaf","Ladybug","Scorpion","Toad"};
+
+        private Dictionary<string, int> EnemyNameDamage = new Dictionary<string, int>()
+        {
+            {"Bat",7},
+            {"Crow",15},
+            {"Leaf",0},
+            {"Scorpion",6},
+            {"Toad",5},
+            {"Ladybug",3}
+        };
+
+        public EnemyGen()
+        {
+            Random rnd = new Random();
+            string drawName = EnemyName[rnd.Next(0,EnemyName.Length)];
+            _name = drawName;
+            _damage = EnemyNameDamage[drawName];
+        }
         // Name has to be Bat related
 
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public int GetDamage()
+        {
+            return _damage;
+        }
 
     }
 }
