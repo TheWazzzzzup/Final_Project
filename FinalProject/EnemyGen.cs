5de5b995de0cbda4bab14f36d971ec06c6fce4af
TheWazzzzzup <amitmoryosef@hotmail.com>
2022-06-14 08:59:30 +0300

    EnemyGen
    enemyGenClass

---
+++ FinalProject/EnemyGen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    static class EnemyGen
+    {
+        static string[] EnemyName = new string[8];
+        // Name has to be Bat 
+
+    }
+}
